c28bceed98b9f0236c76ed759742be1a72d5b027
chinmay.shah <chinmay.shah@acldigital.com>
2026-06-25 11:47:22 +0530

    comments_1

--- git_tutorial.c
+++ git_tutorial.c
@@ -46,6 +46,7 @@
 			$git add .			-(1) - 	//all - to add file in staging area
 			$git rm fileName 	//to remove file from git working dir 	//done-2026.01.28
 								//now that file does not exist on the branch where we have fired this command 
+								//be-careful: it remove file, it does not just remove from git local, but almost deleted the file
 			$git restore --staged fileName //to unstaged a fileName (if, Staged the wrong file) //done-2026.01.28
 	2.2)	$git commit -m "commit message"
 	2.3)	$git commit -a -m "commit message" //to add & commit altogether - but it works on all files - Be-careful
